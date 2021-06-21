70aa09b969d0016125c03bce649eab44c10fa12e
TuanNguyenVanAnh <nvatuan3@gmail.com>
2021-06-21 11:40:08 +0700

    OK

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -2,7 +2,7 @@ import {useState, useEffect} from 'react';
 import Grid from '@material-ui/core/Grid';
 import { backendRoute } from '../../helper';
 import { baseUrl } from '../../helper';
-import { Card, CardMedia, CardActionArea, Typography, Paper, CardContent } from '@material-ui/core';
+import { Card, CardMedia, CardActionArea, Typography, Paper, CardContent, Container } from '@material-ui/core';
 import SoundButton from '../Addon';
 
 function Catview(props) {
@@ -47,30 +47,32 @@ function Leaderboard(props) {
 
     return (
         <>
-            <Grid container spacing={1} justify="center">
-                <Grid item md={8} xs={12}>
-                    <Paper>
-                        <Typography variant="h5" style={{ textAlign: "center" }}>
-                            Top 10 chú mèo đáng yêu nhất vũ trụ
-                        </Typography>
-                    </Paper>
+            <Container>
+                <Grid container spacing={1} justify="center">
+                    <Grid item md={8} xs={12}>
+                        <Paper>
+                            <Typography variant="h5" style={{ textAlign: "center" }}>
+                                Top 10 chú mèo đáng yêu nhất vũ trụ
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid item md={4} xs={12}>
+                        <SoundButton />
+                    </Grid>
+                    {
+                        listCat.map(item => 
+                            <Catview src={baseUrl + item.path} ranking={item.ranking} />
+                        )
+                    }
+                    <Grid item md={12}>
+                        <Paper>
+                            <Typography style={{ textAlign: "center" }}>
+                                Chú mèo nào được chọn nhiều nhất từ tất cả quý vị sẽ được nhảy lên trên đây ngồi á nha.
+                            </Typography>
+                        </Paper>
+                    </Grid>
                 </Grid>
-                <Grid item md={4} xs={12}>
-                    <SoundButton />
-                </Grid>
-                {
-                    listCat.map(item => 
-                        <Catview src={baseUrl + item.path} ranking={item.ranking} />
-                    )
-                }
-                <Grid item md={12}>
-                    <Paper>
-                        <Typography style={{ textAlign: "center" }}>
-                            Chú mèo nào được chọn nhiều nhất từ tất cả quý vị sẽ được nhảy lên trên đây ngồi á nha.
-                        </Typography>
-                    </Paper>
-                </Grid>
-            </Grid>
+            </Container>
         </>
     )
 }
